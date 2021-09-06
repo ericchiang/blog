b4b8f6b23c139e90d9e4119dc75451c86359fd82
Eric Chiang <ericchiang@google.com>
2021-09-06 14:21:05 -0700

    scripts: fix up publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,16 +2,18 @@
 
 cleanup() {
     CODE=$?
-    rm -rf $HOME/ericchiang.github.io
+    rm -rf $TEMP_DIR
     exit $CODE
 }
 
 trap cleanup EXIT
 
+TEMP_DIR="$( mktemp -d )"
+
 SHA=$( git rev-parse HEAD )
 
-REPO=$HOME/ericchiang.github.io
-git clone https://$GITHUB_USERNAME:$GITHUB_PASSWORD@github.com/ericchiang/ericchiang.github.io.git $REPO
+REPO=$TEMP_DIR/ericchiang.github.io
+git clone git@github.com:ericchiang/ericchiang.github.io.git $REPO
 rm -r $REPO/*
 ls -al $REPO
 
@@ -19,11 +21,10 @@ if [ -d public ]; then
     rm -r public
 fi
 
-hugo version
-hugo
+go run github.com/gohugoio/hugo
 rsync -av public/* $REPO/
 cd $REPO
 git add .
 git commit -m "built from $SHA"
 
-git push https://$GITHUB_USERNAME:$GITHUB_PASSWORD@github.com/ericchiang/ericchiang.github.io.git
+git push git@github.com:ericchiang/ericchiang.github.io.git
